c59c968825b52bdd9d00e85f2b279477aa2d2ecb
Jason Lee <lee.jason.park@gmail.com>
2016-03-14 00:07:12 -0700

    adds postgresql support, enables heroku deployment

--- FlaskUrlShortener/urlshortener.py
+++ FlaskUrlShortener/urlshortener.py
@@ -26,8 +26,10 @@ app = Flask(__name__)
 
 # conf
 if isProd:
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    url = urlparse(os.environ["DATABASE_URL"])
+    app.config.update(dict(
+        SECRET_KEY = 'devkey'
+    ))
 else:
     app.config.update(dict(
         DATABASE = os.path.join(app.root_path, 'urls.db'),
@@ -70,26 +72,43 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_all():
-    cur = g.db.execute('select * from urls order by id desc limit 1')
+    if isProd:
+       cur = g.db.cursor()
+       cur.execute('select * from urls order by id desc limit 1')
+       g.db.commit()
+    else:
+       cur = g.db.execute('select * from urls order by id desc limit 1')
     records = [dict(id=row[0], url=row[1], shortened=row[2]) for row in cur.fetchall()]
     return render_template('show_all.html', records=records)
 
 @app.route('/<path:shortened>')
 def find_shortened(shortened):
-    cur = g.db.execute('select url from urls where shortened=?', [shortened])
+    if isProd:
+        cur = g.db.cursor()
+        cur.execute('SELECT url FROM urls where shortened=%s', [shortened]) 
+    else:
+        cur = g.db.execute('select url from urls where shortened=?', [shortened])
     try:
         record = [dict(url=row[0]) for row in cur.fetchall()]
         redirectto = record[0]['url']
         return redirect(redirectto, code=302)
     except:
-        cur = g.db.execute('select * from urls order by id desc limit 1')
+        if isProd:
+            cur = g.db.cursor()
+            cur.execute('SELECT * from urls order by id desc limit 1')
+        else:
+            cur = g.db.execute('select * from urls order by id desc limit 1')
         records = [dict(id=row[0], url=row[1], shortened=row[2]) for row in cur.fetchall()]
         return render_template('show_all.html', records=records)
     
 @app.route('/get', methods=['GET'])
 def get_url():
     requested_shortened = request.args.get('shortened')
-    cur = g.db.execute('select url from urls where shortened=?', [requested_shortened])
+    if isProd:
+        cur = g.db.cursor()
+        cur.execute('SELECT url FROM urls where shortened=%s', [requested_shortened])
+    else:
+        cur = g.db.execute('select url from urls where shortened=?', [requested_shortened])
     try:
         record = [dict(url=row[0]) for row in cur.fetchall()]
         expanded = record[0]['url']
@@ -109,15 +128,27 @@ def add_url():
 
         while True:
             try:
-                # UPSERT-like behavior http://stackoverflow.com/a/15277374
-                g.db.execute('update urls SET url=?, shortened=? where url=?', [stripped_url, stripped_url, untrimmed_shortened[:leftstring_length]])
-                g.db.execute('insert or ignore into urls (url, shortened) values (?, ?)', [stripped_url, untrimmed_shortened[:leftstring_length]])
-                g.db.commit()
+                if isProd:
+                    cur = g.db.cursor()
+                    print("started")
+                    print(leftstring_length)
+                    # crazy upsert CTE from http://stackoverflow.com/a/8702291
+                    cur.execute('WITH new_values (url, shortened) as ( values (%s, %s) ), upsert as ( update urls u set url = nv.url, shortened = nv.shortened FROM new_values nv WHERE u.url = nv.url RETURNING u.* ) INSERT INTO urls (url, shortened) SELECT url, shortened FROM new_values WHERE NOT EXISTS (SELECT 1 FROM upsert up WHERE up.url = new_values.url)', [stripped_url, untrimmed_shortened[:leftstring_length]])
+                    # less safe upsert-like from http://stackoverflow.com/a/6527838
+                    #cur.execute('UPDATE urls SET url=%s, shortened=%s where url=%s', [stripped_url, untrimmed_shortened[:leftstring_length], stripped_url])
+                    #cur.execute('INSERT INTO urls (url, shortened) SELECT %s, %s WHERE NOT EXISTS (SELECT 1 FROM urls where url=%s)', [stripped_url, untrimmed_shortened[:leftstring_length], stripped_url])
+                    g.db.commit()
+                else:
+                    # UPSERT-like behavior http://stackoverflow.com/a/15277374
+                    g.db.execute('UPDATE urls SET url=?, shortened=? WHERE url=?', [stripped_url, untrimmed_shortened[:leftstring_length], stripped_url])
+                    g.db.execute('INSERT OR IGNORE INTO urls (url, shortened) VALUES (?, ?)', [stripped_url, untrimmed_shortened[:leftstring_length]])
+                    g.db.commit()
                 flash('New url was successfully entered')
                 break
             except:
                 # let's get a longer shortened string
                 leftstring_length += 1
+                break
     else:
         flash('Invalid URL for shortening, try again!')
     return redirect(url_for('show_all'))
@@ -127,4 +158,5 @@ def shorten(url):
     return base62_encode(int(m.hexdigest(), 16))
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0')
